ea4b9957836ad8f39cc7e9e6042809a25ffe9627
Luke <lukebbarrier@gmail.com>
2020-09-09 22:52:02 -0400

    Game Start

--- app.js
+++ app.js
@@ -57,13 +57,52 @@ class PlayerSocket {
 	}
 }
 
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+class Deck {
+  constructor() {
+    this.cards = [];
+    for (var i = 2; i < 15; i++) {
+      for (var j = 0; j < 4; j++) {
+        var card = new Card(i, j);
+        this.cards.push(card);
+      }
+    }
+  }
+
+  shuffle() {
+    for (var i in this.cards) {
+      var indexReplace = random(0, this.cards.length);
+      var temp = this.cards[i];
+      this.cards[i] = this.cards[indexReplace];
+      this.cards[indexReplace] = temp;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 io.on("connection", s => {
 	var pSock = new PlayerSocket(s);
 
 	SOCKETS.push(pSock);
 
 	s.pSocket = pSock;
-	s.gameAccount = s.handshake.address;
 
 	s.on("checkValidName", d => {
 		var valid = true;
@@ -177,6 +216,11 @@ io.on("connection", s => {
 	  }
 
 		s.emit("accountList", sortedAccounts);
+	});
+
+	s.on("joinGame", d => {
+		s.player = new Player(d, s.pSocket);
+		PLAYERS.push(s.player);
 	});
 
 	s.on("disconnect", () => {
